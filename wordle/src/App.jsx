207a30ff046cb07474a90ba604af2f6282254750
Kai <kai.ericson@lerniastudent.se>
2025-03-31 10:05:26 +0200

    set up react and vite

---
+++ wordle/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+ 
+ 
+  return (
+    <main className="app">
+      <h1 className="header">WORDLE</h1>
+      
+      <ul className="guessList">
+        <li className="guessItem">ROBOT</li>
+      </ul>
+    </main>
+  )
+}
+
+export default App
