7c4cee430aedcd0584627207401f9e961e6abf2c
Kai <kai.ericson@lerniastudent.se>
2025-04-03 15:40:36 +0200

    added components

--- wordle/src/App.jsx
+++ wordle/src/App.jsx
@@ -1,15 +1,52 @@
-
+import GameSettings from "./components/GameSettings";
+import GuessInput from "./components/GuessInput";
 
 function App() {
- 
+ const items = [ 
+  {
+  label: "ROBOT" ,
+  status:
+    [{letter: "R", result: "misplaced"},
+     {letter: "O", result: "misplaced"},
+     {letter: "B", result: "misplaced"},
+     {letter: "O", result: "misplaced"},
+     {letter: "T", result: "misplaced"}
+    ]
+ },
+ {
+  label: "ROBOT" ,
+  status:
+    [{letter: "R", result: "misplaced"},
+     {letter: "O", result: "misplaced"},
+     {letter: "B", result: "misplaced"},
+     {letter: "O", result: "misplaced"},
+     {letter: "T", result: "misplaced"}
+    ]
+ }
+ ];
+
  
   return (
     <main className="app">
-      <h1 className="header">WORDLE</h1>
-      
-      <ul className="guessList">
-        <li className="guessItem">ROBOT</li>
-      </ul>
+      <div className="game">
+        <h1 className="header">WORDLE</h1>
+        <GameSettings/>
+        <GuessInput/>
+        <h2>Gissningar</h2>
+        <ul className="guessList">
+          {items.map((item) =>{
+           return(
+            <li className="guessItem">
+              {item.status.map((element) =>{
+                <p >{element.letter}</p>
+              })}
+            </li>
+           )
+          }
+          )}
+
+        </ul>
+      </div>
     </main>
   )
 }
